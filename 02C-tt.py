37e3d2f98c8db390114ec11544e532cac2820756
소류 <kjwangel2345@gmail.com>
2019-07-20 19:38:13 +0900

    거북이로 그린 그림 색 지정하기
    2017/07/20

---
+++ 02C-tt.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8
+
+import turtle as t
+
+#삼각형 그리기
+
+t.color("red")
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+#사각형 그리기
+
+t.color("green")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+#원 그리기
+
+t.color("blue")
+t.circle(50)
