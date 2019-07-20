1eef0aa22b858f2486de714637881451c457b987
소류 <kjwangel2345@gmail.com>
2019-07-20 19:18:12 +0900

    거북이로 그림 그리기
    2019/07/20

---
+++ 02B-tt.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8
+
+import turtle as t
+
+#삼각형 그리기
+
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+#사각형 그리기
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+#원 그리기
+
+t.circle(50)
